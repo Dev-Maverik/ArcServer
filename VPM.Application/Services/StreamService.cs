84f9580d034a9ab040bd37f736f843e617102638
Dev-Maverik <tarun.undhad@matrixcomsec.com>
2024-08-12 12:13:37 +0530

    Add project files.

---
+++ VPM.Application/Services/StreamService.cs
@@ -0,0 +1,112 @@
+﻿using VPM.Application.Interfaces;
+using VPM.Application.Models;
+
+namespace VPM.Application.Services
+{
+
+    public class StreamService : IStreamService
+    {
+
+        #region Declaration
+
+        StreamManager streamManager;
+
+        #endregion
+
+        #region Ctor
+
+        public StreamService()
+        {
+            streamManager = new StreamManager();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string GetStreamUrl(string url)
+        {
+            string responseUrl = string.Empty;
+
+            try
+            {
+                if (streamManager.CreatePipe(url))
+                {
+                    StremMapping? mapping = streamManager.GetMapping(url);
+
+                    if (mapping != null)
+                    {
+                        streamManager.Play(mapping.PipeLineId);
+                        responseUrl = mapping.WebRtcUrl;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return responseUrl;
+        }
+
+        public bool StartStream(string url)
+        {
+            bool response = false;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    streamManager.Play(streamManager.GetMapping(url)?.PipeLineId ?? 0);
+                    response = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return response;
+        }
+
+        public bool PauseStream(string url)
+        {
+            bool response = false;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    streamManager.Play(streamManager.GetMapping(url)?.PipeLineId ?? 0);
+                    response = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return response;
+        }
+
+        public bool StopStream(string url)
+        {
+            bool response = false;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    streamManager.Play(streamManager.GetMapping(url)?.PipeLineId ?? 0);
+                    response = true;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return response;
+        }
+
+        #endregion
+
+    }
+
+}
